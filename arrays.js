de13bc84c6c77aaee0bb4642e5b33d32de320813
Johnathon Ammon <johnathonammon@Johnathons-MacBook-Pro.local>
2022-09-21 10:22:36 -0600

    finish this challelnge

--- arrays.js
+++ arrays.js
@@ -1,5 +1,23 @@
-var friends = ["sarah", "steff", "rachael", "james", "andy", ];
+var friends = ["sarah", "steff", "rachael", "james", "andy" ];
 
-var boxs = [ 29, 42, 78, 60, 104, ];
+var boxes = [ 29, 42, 78, 60, 104 ];
 
-var perspective [ true, false, true, false, false, true, ];
+Boolean bools[ ] = new Boolean[4];
+bools[2] = true;
+bools[4] = true;
+bools[6] = true;
+bools[9] = false;
+
+for(int i = 0 ;i < bools.langth ; i++)
+
+{ systome.out.println(bools[i]);
+
+
+
+}
+
+boxes.pop();
+console.log(boxes);
+
+friends2 = friends.slice(2);
+console.log(friends2);
