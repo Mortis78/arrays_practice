d52253bd14209543b71ad0c3fd52ce5c9bcdbb41
Johnathon Ammon <johnathonammon@Johnathons-MacBook-Pro.local>
2022-09-21 10:32:12 -0600

    share my thoughts

--- arrays.js
+++ arrays.js
@@ -2,22 +2,24 @@ var friends = ["sarah", "steff", "rachael", "james", "andy" ];
 
 var boxes = [ 29, 42, 78, 60, 104 ];
 
-Boolean bools[ ] = new Boolean[4];
+var bools = [0, 1, 2, 3, 4, 5];
+// ive set 2 and 4 values to true. 0,3,5 have been set to false
+bools[0] = false;
+bools[1] = false;
 bools[2] = true;
+bools[3] = false;
 bools[4] = true;
-bools[6] = true;
-bools[9] = false;
+bools[5] = false;
+// when called the true/false value should be printed
+console.log(bools);
 
-for(int i = 0 ;i < bools.langth ; i++)
-
-{ systome.out.println(bools[i]);
-
-
-
-}
-
+//this will remove the last number from the array "boxes"
+//when it prints, 104 will be gone
 boxes.pop();
+//prints new array for boxes
 console.log(boxes);
 
+//this will remove the first two strings in my array.
 friends2 = friends.slice(2);
+//prints my updated array.
 console.log(friends2);
---
+++ self-evaluation.md
@@ -0,0 +1,7 @@
+# so far?
+[I thought arrays is something your boss gave you...$who knew :) ]
+I felt pretty good so far, it took me a bit to get the bools to work the way i wanted but that was do to over complication on my part.
+## additional learning?
+i just need to spend more time working with them, but i have a feeling there will be lots of that.
+### questions
+non yet, but we are about to do the group learning so im sure after that ill have plenty.
